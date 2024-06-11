2ac837a1d7ed69679dbe2badd952d8a29fab3d2c
CintiaMariana <81639655+CintiaMariana@users.noreply.github.com>
2024-06-11 20:55:36 -0300

    Add files via upload

---
+++ test/SupplierIntegrationTest.java
@@ -0,0 +1,8 @@
+package br.edu.univas.Supplier.test;
+
+public class SupplierIntegrationTest {
+	
+	private ObjectMapper mapper = new ObjectMapper();
+	private final String supplierURL = "http://localhost:8080/api/supplier";
+
+}
